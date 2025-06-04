9741b8a1ffa2df9c7ce54b83b085b7b74407b43e
arthurMoreiraC <a2024951729@teiacoltec.org>
2025-06-04 15:08:51 -0300

    exercicio 14 da 10 lista grande adicionado

---
+++ exer10_14.m
@@ -0,0 +1,12 @@
+#Arthur Moreira De Oliveira | 205
+
+n = 10;
+
+s = 0;
+
+for i = -n:n
+  if (i + 1 == 0 || i - 1 == 0) continue endif
+  s = ((2*i + 1)/(i + 1)) + ((i + 1)/(i - 1)) + (i * sin((2*pi) * i ));
+endfor
+
+printf("O valor final de S eh: %f\n", s);
