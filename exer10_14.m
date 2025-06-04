889431e406cfcd41954f75c4dbcce2a0be8fb8a2
arthurMoreiraC <a2024951729@teiacoltec.org>
2025-06-04 15:10:52 -0300

    erro corrigido

--- exer10_14.m
+++ exer10_14.m
@@ -6,7 +6,7 @@ s = 0;
 
 for i = -n:n
   if (i + 1 == 0 || i - 1 == 0) continue endif
-  s = ((2*i + 1)/(i + 1)) + ((i + 1)/(i - 1)) + (i * sin((2*pi) * i ));
+  s +=((2*i + 1)/(i + 1)) + ((i + 1)/(i - 1)) + (i * sin((2*pi) * i ));
 endfor
 
 printf("O valor final de S eh: %f\n", s);
