916df2d51687a3a02119fd90c4359dc603478cc9
arthurMoreiraC <a2024951729@teiacoltec.org>
2025-08-05 13:14:56 -0300

    Cria a classe circulo

---
+++ POOUse.m
@@ -0,0 +1,6 @@
+% Criar um objeto da classe Circulo com raio 5
+c1 = Circulo(5);
+
+% Calcular e exibir a área e a circunferência
+disp(['Área do círculo: ', num2str(c1.calcularArea())]);
+disp(['Circunferência do círculo: ', num2str(c1.calcularCircunferencia())]);
